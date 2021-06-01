1bf29e3da1f83c0632a545e7dc06138713e70a36
Leo Ramírez M <leo.ramirez.m@hotmail.com>
2021-05-31 21:37:29 -0500

    Role

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { RoleRepository } from '../role/Role.repository';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository])],
+    imports: [
+        TypeOrmModule.forFeature([UserRepository, RoleRepository]),
+        AuthModule
+    ],
     providers: [UserService],
     controllers: [UserController]
 })
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,10 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserDto } from './dto/create-user.dto';
+import { Status } from 'src/shared/status.enum';
+import { getConnection } from 'typeorm';
+import { Role } from '../role/role.entity';
+import { RoleRepository } from '../role/Role.repository';
+import { RoleType } from '../role/roletype.enum';
 import { UserDetails } from './user.details.entity';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
@@ -9,7 +13,9 @@ import { UserRepository } from './user.repository';
 export class UserService {
     constructor(
         @InjectRepository(UserRepository)
-        private readonly _userRepository: UserRepository
+        private readonly _userRepository: UserRepository,
+        @InjectRepository(RoleRepository)
+        private readonly _roleRepository: RoleRepository
     ) {}
 
     async get(id: number): Promise<User> {
@@ -18,7 +24,7 @@ export class UserService {
         }
 
         const user: User = await this._userRepository.findOne(id, {
-            where: { status: 'ACTIVE' },
+            where: { status: Status.ACTIVE },
         });
 
         if (!user) {
@@ -30,7 +36,7 @@ export class UserService {
 
     async getAll(): Promise<User[]> {
         const users: User[] = await this._userRepository.find({
-            where: { status: 'ACTIVE' },
+            where: { status: Status.ACTIVE },
         });
         
         return users;
@@ -39,8 +45,12 @@ export class UserService {
     async create(user: User): Promise<User> {
         const details = new UserDetails();
         user.details = details;
-        
-        const savedUser = await this._userRepository.save(user);
+
+        const repo = await getConnection().getRepository(Role);
+        const defaultRole = await repo.findOne({ where: { name: RoleType.GENERAL } });
+        user.roles = [defaultRole];
+
+        const savedUser: User = await this._userRepository.save(user);
 
         return savedUser;
     }
@@ -51,13 +61,36 @@ export class UserService {
 
     async delete(id: number): Promise<void> {
         const userExists = await this._userRepository.findOne(id, {
-            where: { status: 'ACTIVE' }
+            where: { status: Status.ACTIVE }
         });
 
         if (!userExists) {
             throw new NotFoundException();
         }
 
-        await this._userRepository.update(id, {status: 'INACTIVE'});
+        await this._userRepository.update(id, {status: Status.INACTIVE});
+    }
+
+    async setRoleToUser(userId: number, roleId: number) {
+        const userExist = await this._userRepository.findOne(userId, {
+          where: { status: Status.ACTIVE },
+        });
+    
+        if (!userExist) {
+          throw new NotFoundException();
+        }
+    
+        const roleExist = await this._roleRepository.findOne(roleId, {
+          where: { status: Status.ACTIVE },
+        });
+    
+        if (!roleExist) {
+          throw new NotFoundException('Role does not exist.');
+        }
+    
+        userExist.roles.push(roleExist);
+        await this._userRepository.save(userExist);
+    
+        return true;
     }
 }
